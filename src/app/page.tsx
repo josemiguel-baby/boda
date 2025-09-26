6c25d4eeb4b542ff2b20ce7a7cbb1057138f2a60
José Carreño <josec6360@gmail.com>
2025-09-26 01:54:35 +0000

    add mobile view padding again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   const weddingDate = new Date("2025-12-20T16:00:00");
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center px-4">
       <Hero />
       <OurStory />
       <Separator className="my-12 md:my-20" />
