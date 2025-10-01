dbd52ae205539f562fd3f5a9a88b552bbe48c89d
José Carreño <josec6360@gmail.com>
2025-10-01 15:54:58 +0000

    Change the page tittle from the project to: Yeremy y María

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 
+
 "use client";
 
 import type { Metadata } from 'next';
@@ -6,6 +7,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { useState, useEffect } from 'react';
 import { Loader } from '@/components/shared/Loader';
+
 
 export default function RootLayout({
   children,
@@ -23,9 +25,14 @@ export default function RootLayout({
     return () => clearTimeout(timer);
   }, []);
 
+  useEffect(() => {
+    document.title = "Yeremy y María";
+  }, []);
+
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+      <link rel="icon" href="images/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
