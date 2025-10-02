92077e0a3981d29f33629303ae760068d4c28c29
José Carreño <josec6360@gmail.com>
2025-10-02 15:29:24 +0000

    Por favor coloca el icono svg al lado izquierdo del texto que dice restr

--- src/components/invitation/DressCode.tsx
+++ src/components/invitation/DressCode.tsx
@@ -7,30 +7,45 @@ export function DressCode() {
 
   return (
     
-    <Section id="dress-code" title="Código de vestimenta" className="bg-cover bg-no-repeat rounded-lg shadow-lg bg-top" style={{ backgroundImage: "url('/images/fondo.jpg')" }}>
-        <div className=" max-w-2xl mx-auto text-center space-y-6 p-4">
-          
-          <p className="font-headline text-2xl md:text-3xl font-bold tracking-tight text-primary">
-          <img src="/images/detalle.png" className="w-4 inline-block mx-2"></img>
-          Coctel
-  <img src="/images/detalle.png" className="w-4 inline-block mx-2" ></img>
-          </p>
-          
-          <p className="text-lg text-muted-foreground">
-          Amablemente les pedimos que cumplan con la vestimenta sugerida para que todos luzcan hermosos en nuestras fotos del recuerdo.
-          </p>
-
-          <p className="text-lg text-muted-foreground">
-          Aqui puedes ver un enlace con ideas semejantes a lo que queremos.
-          </p>
-          <Button asChild>
-            <a href={pinterestLink} target="_blank" rel="noopener noreferrer">
-              <Shirt className="mr-2 h-4 w-4" />
-              Ver ideas
-            </a>
-          </Button>
+    <Section id="dress-code" title="Código de vestimenta" className="bg-cover bg-no-repeat rounded-lg shadow-lg bg-bottom" style={{ backgroundImage: "url('/images/fondo.jpg')" }}>
         
+        <div className="bg-background/70 p-4 md:p-8 rounded-lg shadow-lg">
+          <div className=" max-w-2xl mx-auto text-center space-y-6 p-4">
+            <p className="font-headline text-3xl md:text-4xl font-bold tracking-tight text-primary">
+            <img src="/images/detalle.png" className="w-3 inline-block mx-2"></img>
+            Coctel
+            <img src="/images/detalle.png" className="w-3 inline-block mx-2" ></img>
+            </p>
+            
+            <p className="text-lg text-muted-foreground">
+            Amablemente les pedimos que cumplan con la vestimenta sugerida para que todos luzcan hermosos en nuestras fotos del recuerdo.
+            </p>
+            
+            <div className="text-lg text-muted-foreground space-y-2">
+              <div className="flex items-center justify-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+                </svg>
+                <span>Restricciones:</span>
+              </div>
+              <ul className="list-none p-0">
+                <li>No Blue Jeans</li>
+                <li>No zapatos de tacón de punta</li>
+                <li>No franelas</li>
+              </ul>
+            </div>
 
+            <p className="text-lg text-muted-foreground">
+            Aqui puedes ver un enlace con ideas semejantes a lo que queremos.
+            </p>
+            
+            <Button asChild>
+              <a href={pinterestLink} target="_blank" rel="noopener noreferrer">
+                <Shirt className="mr-2 h-4 w-4" />
+                Ver ideas
+              </a>
+            </Button>
+          </div>
         </div>
     </Section>
   );
